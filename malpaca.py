1aa37da9ccd8355543bcab3057b82466c94978c3
azqa <azqa.nadeem@gmail.com>
2020-12-10 23:21:38 +0600

    Auto-gen DAG with paramless dist

--- malpaca.py
+++ malpaca.py
@@ -603,7 +603,7 @@ def connlevel_sequence(metadata, mapping):
         graph = {}
         names ={}
         for line in csv_reader:
-            graph[line[0]] = []
+            graph[line[0]] = set()
             if line[0] not in names.keys():
                 names[line[0]] = []
             names[line[0]].append(line[1]+"("+line[2]+")")
@@ -615,61 +615,111 @@ def connlevel_sequence(metadata, mapping):
 
         zeros = ''.join(['0']*(numclus-1))
 
+        specials  = []
+    
         next.append(zeros)
         while(len(next)>0):
             l1 = next.popleft()
             covered.add(l1)
             for l2 in ulist:
-                if l2 not in covered and difference(l1,l2) == 1:
-                    graph[l1].append(l2)
+                if l2 not in covered and difference(l1,l2) == 1:                  
+                    graph[l1].add(l2)
+                
                     if l2 not in next:
                         next.append(l2)
-        #_keys = graph.keys()
-        val = []
+
+        #keys = graph.keys()
+        val = set()
         for v in graph.values():
-            val.extend(v)
-        specials  = []
-        for u in ulist:
+            val.update(v)
 
-            if u not in val and u != zeros:
-                graph[zeros].append(u)
-                specials.append(u)
-        f2 = open('relation-tree'+addition+'.dot', 'w')
-        f2.write("digraph dag {\n")
-        f2.write("rankdir=LR;\n")
-        num = 0
-        for idx,li in names.items():
-            text = ''
-            #print(idx)
-            name = str(idx)+'\n'
-            
-            for l in li:
-                name+=l+',\n'
-            #print(str(idx) + " [label=\""+str(num)+"\"]")
-            if idx not in specials:
-                print(str(idx) + " [label=\""+name+"\"]")
-                text = str(idx) + " [label=\""+name+"\" , shape=box;]"
+        notmain = [x for x in ulist if x not in val]
+        notmain.remove(zeros)
+        nums = [sum([int(y) for y in x]) for x in notmain]
+        notmain = [x for _,x in sorted(zip(nums,notmain))]
+
+        specials = notmain
+        print(notmain)
+        print(len(notmain))
+
+
+
+        extras = set()
+        
+        for nm in notmain:
+            comp = set()
+            comp.update(val)
+            comp.update(extras)
+
+            mindist = 1000
+            minli1, minli2 = None, None
+            for l in comp:
+                if nm != l:
+                    diff  = difference(nm,l)
+                    if diff < mindist:
+                        mindist = diff
+                        minli = l
+
+            diffbase = difference(nm,zeros)
+            #print('diffs', nm, 'extra', mindist, 'with root', diffbase)
+            if diffbase <= mindist:
+                mindist = diffbase
+                minli = zeros
+                #print('replaced')
+                
+                    
+
+            num1 = sum([int(s) for s in nm])
+            num2 = sum([int(s) for s in minli])
+            if num1 < num2:
+                graph[nm].add(minli)
             else:
-                print(str(idx) + " [style=\"filled\" fillcolor=\"red\" label=\""+name+"\"]")
-                text = str(idx) + " [style=\"filled\" shape=box, fillcolor=\"red\" label=\""+name+"\"]"
+                graph[minli].add(nm)
+            
+            
+            extras.add(nm)
 
-            f2.write(text)
-            f2.write('\n')
-        for k,v in graph.items():
-            for vi in v:
-                f2.write(str(k)+"->"+str(vi))
+
+        #keys = graph.keys()
+        val = set()
+        for v in graph.values():
+            val.update(v)     
+            f2 = open('relation-tree'+addition+'.dot', 'w')
+            f2.write("digraph dag {\n")
+            f2.write("rankdir=LR;\n")
+            num = 0
+            for idx,li in names.items():
+                text = ''
+                #print(idx)
+                name = str(idx)+'\n'
+                
+                for l in li:
+                    name+=l+',\n'
+                #print(str(idx) + " [label=\""+str(num)+"\"]")
+                if idx not in specials:
+                    print(str(idx) + " [label=\""+name+"\"]")
+                    text = str(idx) + " [label=\""+name+"\" , shape=box;]"
+                else:
+                    print(str(idx) + " [style=\"filled\" fillcolor=\"red\" label=\""+name+"\"]")
+                    text = str(idx) + " [style=\"filled\" shape=box, fillcolor=\"red\" label=\""+name+"\"]"
+
+                f2.write(text)
                 f2.write('\n')
-                print(k+"->"+vi)
-        f2.write("}")
-        f2.close()
-    # Rendering DAG
-    print('Rendering DAG -- needs graphviz dot')
-    try:
-        os.system('dot -Tpng relation-tree'+addition+'.dot -o DAG'+addition+'.png')
-        print('Done')
-    except:
-        print('Failed')
-        pass
+            for k,v in graph.items():
+                for vi in v:
+                    f2.write(str(k)+"->"+str(vi))
+                    f2.write('\n')
+                    print(k+"->"+vi)
+            f2.write("}")
+            f2.close()
+        # Rendering DAG
+        print('Rendering DAG -- needs graphviz dot')
+        try:
+            os.system('dot -Tpng relation-tree'+addition+'.dot -o DAG'+addition+'.png')
+            print('Done')
+        except:
+            print('Failed')
+            pass
 
 
     # temporal heatmaps start
